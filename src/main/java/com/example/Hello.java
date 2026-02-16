5d62db08d20c16fa5dc6ae26af0bdbb52f96dfeb
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2026-02-16 23:26:52 +0530

    Hello.java maven

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
